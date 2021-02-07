f0111ef89a91dba68806921fae6aa91eca9623f3
Marian Maschke <marianlmaschke@googlemail.com>
2021-02-07 23:16:43 +0900

    added instructions for development system set up

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text><h1>HELLO!</h1></Text>
+      <Text>HELLO!LOL!</Text>
       <StatusBar style="auto" />
     </View>
   );
